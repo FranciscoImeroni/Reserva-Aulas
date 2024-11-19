a8ac4728d134117d01905359125810ee7c40bb88
Franime <franciscoimeroni@mail.com>
2024-11-19 03:19:24 -0300

    Base Front

---
+++ front/reserva_aulas-front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Bienvenido a mi aplicación</h1>
+      <p>Este es el inicio del frontend de mi proyecto.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
